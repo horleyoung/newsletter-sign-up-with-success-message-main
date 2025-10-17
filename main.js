9993bfe7637a939faeb060247172a511123682f0
Adepoju Habeeblah Olamilekan <126694613+horleyoung@users.noreply.github.com>
2025-10-17 04:46:00 -0700

    email validation form

---
+++ main.js
@@ -0,0 +1,32 @@
+
+const submitBox = document.querySelector(".submit");
+const messageContainer = document.getElementById("message-container");
+const mainBox = document.querySelector(".main");
+const emailInput = document.getElementById("email");
+const errorMessage = document.querySelector(".error-message");
+
+messageContainer.classList.add('invisible');
+
+submitBox.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  // Check email validity first
+  if (!emailInput.checkValidity()) {
+    errorMessage.classList.remove("invisible");
+    emailInput.classList.add("error-border");
+    return;
+  }
+
+  // If valid, remove any old error state
+  errorMessage.classList.add("invisible");
+  emailInput.classList.remove("error-border");
+
+  //  Then ask for confirmation
+  const confirmSubmit = confirm('Do you really want to submit the form?');
+  if (!confirmSubmit) return;
+
+  // Finally, hide form and show success message
+  mainBox.classList.add('invisible');
+  messageContainer.classList.remove('invisible');
+});
+
